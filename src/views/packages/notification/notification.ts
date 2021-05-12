8d29fa0f3202f716b8ed8ef8ed9561ef98f6475e
bin000121 <867398019@.com>
2021-05-12 23:23:08 +0800

    'fix-bug'

--- src/views/packages/notification/notification.ts
+++ src/views/packages/notification/notification.ts
@@ -81,7 +81,7 @@ const notifyInstance: NotifyType = ((options: Options) => {
         return
     }
     let placement = options?.placement || 'top-right'
-    // 一个方向上的消息在页面中最多存在10条
+    // 一个方向上的消息在页面中最多存在5条
     if (instance[placement].length >= 5) {
         console.warn(`[fast-ui]: There are up to 10 messages in the ${placement} corner of the page!`)
         return
