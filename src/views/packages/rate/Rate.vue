aee7ae5e72875c6317db381afe382fd533cbee66
bin000121 <867398019@.com>
2021-09-20 12:35:33 +0800

    ''

--- src/views/packages/rate/Rate.vue
+++ src/views/packages/rate/Rate.vue
@@ -1,14 +1,28 @@
 <template>
-    <div class="f-rate">
+    <div
+        class="f-rate"
+        ref="fRate"
+    >
         <span
             v-for="num in countNum"
             :key="num"
             :class="{
                 [icon]: true,
-                'f-rate-item': true
+                'f-rate-item': true,
+                'f-rate-item_selected': currentIdx >= (num -1)
             }"
-            :style="getStyle"
+            :style="getFs"
+            @mouseenter="handleMouseenter(num - 1)"
+            @mouseleave="handleMouseleave(num - 1)"
+            @click="handleClick(num - 1)"
         >
+        </span>
+        <span
+            v-if="!showLabel"
+            class="f-rate-label"
+            :style="getFs"
+        >
+            {{ '5.0' }}
         </span>
     </div>
 </template>
@@ -20,7 +34,7 @@
         ref,
         onMounted
     } from 'vue'
-
+    import { debounce } from '/@/utils/utils'
     export default defineComponent({
         props: {
             halfSelect: {
@@ -48,20 +62,59 @@
                 type: [String, Number],
                 default: 20,
             },
-            color: {
+            selectedColor: {
                 type: String,
                 default: '#fd7716'
+            },
+            noSelectedColor: {
+                type: String,
+                default: '#ddd'
+            },
+            showLabel: {
+                type: Boolean,
+                default: false
+            },
+            labelPlacement: {
+                type: String,
+                default: 'right',
+                validator: (val: string) => ['right', 'left'].includes(val)
             }
         },
         setup (props) {
-            const getStyle = computed(() => {
+            const fRate = ref(null)
+            let fRateDom: HTMLElement
+            const currentIdx = ref(-1)
+            const getFs = computed(() => {
                 return {
                     fontSize: typeof props.fontSize === 'string' ? props.fontSize : props.fontSize + 'px',
-                    color: props.color
                 }
             })
+
+            const handleMouseenter = (idx: number) => {
+                currentIdx.value = idx
+            }
+
+            const handleMouseleave = () => {
+                currentIdx.value = -1
+            }
+
+            const handleClick = (idx: number) => {
+                console.log(idx)
+            }
+
+            onMounted(() => {
+                fRateDom = fRate.value!
+                fRateDom.style.setProperty('--rate-height', getFs.value.fontSize)
+                fRateDom.style.setProperty('--selected-color', props.selectedColor)
+                fRateDom.style.setProperty('--no-selected-color', props.noSelectedColor)
+            })
             return{
-                getStyle
+                getFs,
+                fRate,
+                currentIdx,
+                handleMouseenter,
+                handleMouseleave,
+                handleClick,
             }
         }
     })
@@ -69,13 +122,33 @@
 
 <style scoped lang="scss">
 .f-rate{
+    --rate-height: 16px;
+    --selected-color: #fd7716;
+    --no-selected-color: #ddd;
     display: inline-block;
+    height: calc(1.5 * var(--rate-height));
+    line-height: calc(1.5 * var(--rate-height));
 }
 .f-rate-item{
+    display: inline-block;
     cursor: pointer;
+    transition: transform .2s ease;
+    color: var(--no-selected-color);
+    &:hover{
+        transform: scale(1.25);
+    }
+}
+
+.f-rate-item_selected{
+    color: var(--selected-color)!important;
 }
 
 .f-rate-item + .f-rate-item{
-    margin-left: 8px;
+    padding-left: calc(.5 * var(--rate-height));
+}
+
+.f-rate-label{
+    color: #666;
+    margin-left: 15px;
 }
 </style>
